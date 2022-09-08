fc4fb717bf8754fadb1912518fecfdc2a827c835
rioarisandi20 <112737124+rioarisandi20@users.noreply.github.com>
2022-09-07 19:58:40 -0700

    Add files via upload

---
+++ luas persegi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int sisi,luas;
+	
+	cout<<"Masukkan sisi persegi : ";
+	cin>>sisi;
+	
+	luas = sisi*sisi;
+	cout<<"luas Persegi adalah "<<luas;
+	return 0;
+}
