ae697c30688660d9173a5d8c1748f51b53ee07f9
rioarisandi20 <112737124+rioarisandi20@users.noreply.github.com>
2022-09-07 19:59:06 -0700

    Add files via upload

---
+++ kll prsgi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int sisi,keliling;
+	
+	cout<<"Masukkan sisi persegi : ";
+	cin>>sisi;
+	
+	keliling = 4*sisi;
+	cout<<"Keliling Persegi adalah "<<keliling;
+	return 0;
+}
