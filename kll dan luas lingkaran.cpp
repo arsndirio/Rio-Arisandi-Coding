8886b374d9c4220db13e163a7bcc05d11c2b9956
rioarisandi20 <112737124+rioarisandi20@users.noreply.github.com>
2022-09-10 20:08:00 -0700

    Add files via upload

---
+++ kll dan luas lingkaran.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	// l = 3.14*r*r
+	// K = 2*3.14*r
+	double r, K, L;
+	cout<<"masukkan r : "; cin>> r ;
+	L = 3.14*r*r;
+	K = 2*3.14*r;
+	cout<<"L = "<<L<<endl;
+	cout<<"K = "<<K<<endl;
+}
+	
